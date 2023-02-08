066a057ea0775ea456930b615dd6c6f530e40efc
Amy Spears <amspears007@gmail.com>
2023-02-08 10:12:41 -0700

    Add add_car method and tests

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -1,6 +1,15 @@
-class Dealership
+class Dealership  
+  attr_reader :name, 
+              :address,
+              :inventory
 
   def initialize(name, address)
+    @name = name
+    @address = address
+    @inventory = []
+  end
 
+  def add_car(car)
+    inventory << car
   end
 end
--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -7,4 +7,22 @@ RSpec.describe Dealership do
 
     expect(dealership).to be_a(Dealership)
   end
+
+  it 'starts with no inventory' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+
+    expect(dealership.inventory).to eq([])
+    expect(dealership.inventory.count).to eq(0)
+  end
+
+  it 'can have inventory added' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    car_1 = Car.new("Ford Mustang", 1500, 36)
+    car_2 = Car.new("Toyota Prius", 1000, 48)
+
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+
+    expect(dealership.inventory).to match_array([car_1, car_2])
+  end
 end
